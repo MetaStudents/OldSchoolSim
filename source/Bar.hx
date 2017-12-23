c52012c2bae3c522bdfdf8d249d17be8c525a19b
Joshua Meyers <meygerjos@gmail.com>
2017-12-23 04:55:29 -0500

    Encapsulate functions in Bar and refactor code

--- source/Bar.hx
+++ source/Bar.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.FlxG;
 //import source.AssetPaths;
 ///import flixel.util.FlxColor;
 
@@ -10,14 +11,25 @@ import flixel.FlxSprite;
  */
 class Bar extends FlxSprite
 {
-
-	public function new(?X:Float=0, ?Y:Float=0) 
+    private var interestRate:Float;
+	
+	public function new(interestRate:Float, ?X:Float=0, ?Y:Float=0) 
 	{
 		super(X, Y);
 		//makeGraphic(64, 16, FlxColor.BLACK);
 		loadGraphic("assets/images/chimptail.png", false, 74, 65);
 		
-		
+		this.interestRate = interestRate;
+	}
+	
+	public function accrueInterest(elapsed:Float)
+	{
+		y -= interestRate * elapsed * (FlxG.height - y);
+	}
+	
+	public function work (elapsed:Float, workRate:Float)
+	{
+		y += workRate * elapsed;
 	}
 	
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,42 +7,35 @@ import flixel.FlxG;
 
 class PlayState extends FlxState
 {
-	private var coef1:Float;
-	private var coef2:Float;
-	private var coef3:Float;
-	private var workrate:Float;
+	private var workRate:Float;
 	private var bar1:Bar;
 	private var bar2:Bar;
 	private var bar3:Bar;
 	override public function create():Void
 	{
 		bgColor = FlxColor.WHITE;
-		bar1 = new Bar(200, 400);
+		bar1 = new Bar(0.03, 200, 400);
 		add(bar1);
-		bar2 = new Bar(300, 400);
+		bar2 = new Bar(0.06, 300, 400);
 		add(bar2);
-		bar3 = new Bar(400, 400);
+		bar3 = new Bar(0.09, 400, 400);
 		add(bar3);
-		coef1 = 0.03;
-		coef2 = 0.06;
-		coef3 = 0.09;
-		workrate = 30;
+		workRate = 30;
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		bar1.y -= coef1 * elapsed * (FlxG.height - bar1.y);
-		bar2.y -= coef2 * elapsed * (FlxG.height - bar2.y);
-		bar3.y -= coef3 * elapsed * (FlxG.height - bar3.y);
-		trace(bar3.y);
+		bar1.accrueInterest(elapsed);
+		bar2.accrueInterest(elapsed);
+		bar3.accrueInterest(elapsed);
 		
 		if (FlxG.mouse.overlaps(bar1) && FlxG.mouse.pressed)
-		    bar1.y += workrate * elapsed;
+		    bar1.work(elapsed, workRate);
 		if (FlxG.mouse.overlaps(bar2) && FlxG.mouse.pressed)
-		    bar2.y += workrate * elapsed;
+		    bar2.work(elapsed, workRate);
 		if (FlxG.mouse.overlaps(bar3) && FlxG.mouse.pressed)
-		    bar3.y += workrate* elapsed;
+		    bar3.work(elapsed, workRate);
 	}
 }
