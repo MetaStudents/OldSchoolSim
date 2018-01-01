1f8e55cc240ef58eed4e8bf5094d8bf428b7df56
Joshua Meyers <meygerjos@gmail.com>
2018-01-01 03:57:36 -0500

    Make Exam class into typedef

--- source/Exam.hx
+++ source/Exam.hx
@@ -4,19 +4,9 @@ package;
  * ...
  * @author test
  */
-class Exam 
-{
-
-	private var startTime:Time;
-	private var endTime:Time;
-	private var size:Float;
-	private var curve:Float;  //1 means no curve
-	public function new(startTime:Time, endTime:Time, size:Float, ?curve:Float=1) 
-	{
-		this.startTime = startTime;
-		this.endTime = endTime;
-		this.size = size;
-		this.curve = curve;
-	}
-	
+typedef Exam = {
+	var startTime:Time;
+	var endTime:Time;
+	var size:Float;
+	var curve:Float;  //1 means no curve
 }
